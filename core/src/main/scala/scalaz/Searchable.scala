93c919d0ef739c361d8c78ed75cc6c152112b9f4
Tony Morris <tmorris@tmorris.net>
2014-10-06 11:12:23 +1000

    Searchable

--- core/src/main/scala/scalaz/Searchable.scala
+++ core/src/main/scala/scalaz/Searchable.scala
@@ -3,19 +3,36 @@ package scalaz
 trait Searchable[A] {
   def search[B](run: A => Maybe[B]): Maybe[B]
 
-  def map[C](f: A => C): Searchable[C] = new Searchable[C] {
-    def search[B](run: C => Maybe[B]) =
-      Searchable.this.search(run compose f)
-  }
+  def map[C](f: A => C): Searchable[C] =
+    new Searchable[C] {
+      def search[B](run: C => Maybe[B]) =
+        Searchable.this.search(run compose f)
+    }
 
-  def flatMap[C](f: A => Searchable[C]): Searchable[C] = new Searchable[C] {
-    def search[B](run: C => Maybe[B]) =
-      Searchable.this.search(f(_).search(run))
-  }
+  def flatMap[C](f: A => Searchable[C]): Searchable[C] =
+    new Searchable[C] {
+      def search[B](run: C => Maybe[B]) =
+        Searchable.this.search(f(_).search(run))
+    }
 
   def exists(p: A => Boolean): Boolean = 
     search(a => if(p(a)) Maybe.just(()) else Maybe.empty[Unit]).isJust
   
   def forall(p: A => Boolean): Boolean =
     !exists(!p(_))
+
+  def maybe: Searchable[Maybe[A]] =
+    new Searchable[Maybe[A]] {
+      def search[B](run: Maybe[A] => Maybe[B]) =
+        run(Maybe.empty) orElse Searchable.this.search(a => run(Maybe.just(a)))
+        // search (mamb . Just)
+    }
+}
+
+object Searchable {
+  def unsearch[A](m: Maybe[A]): Searchable[A] =
+    new Searchable[A] {
+      def search[B](run: A => Maybe[B]) =
+        m flatMap run
+    }
 }
